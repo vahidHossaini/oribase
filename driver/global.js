3b8e74bbb74944e6ae8714d65e8f26fc90912e4b
vahid hossaini <vahid.hossaini@gmail.com>
2020-01-18 19:08:38 +0330

    v007

--- driver/global.js
+++ driver/global.js
@@ -4,7 +4,26 @@ module.exports = class globalClass
 {
 	constructor()
 	{
-		
+        global.ori={};
+		global.ori.Random=function(min,max)
+        {
+            var r = Math.floor(Math.random() * (max - min + 1) + min); 
+            return r
+            
+        }
+		global.ori.RandomInt=function(n=5)
+        {
+            var str=''
+            var sr='23456890'
+            for(var a=0;a<n;a++)
+            {
+                var i=global.ori.Random(0,sr.length-1)
+                str+=sr[i]
+            }
+            if(str[0]=='0')
+                str='1'+str
+            return parseInt(str)
+        }
         String.prototype.replaceAll = function(search, replacement) {
           var target = this;
           return target.replace(new RegExp(search, 'g'), replacement);
--- driver/web.js
+++ driver/web.js
@@ -3,20 +3,69 @@ module.exports = class web
 {
     constructor()
     { 
-	
-    }
-    get(url,func)
+        global.web=this
+    } 
+    getData(url,data,header,func)
     {
+        if(url.indexOf('?')==-1 && data)
+        {
+            url+="?"
+        }
+        if(data)
+            for(var a in data)
+            {
+                url+="&"+a+'='+data[a];
+            }
+        var options = { method: 'GET',
+            url: url, 
+            json: true };
+        if(header)
+            options.header=header;
         if(func)
         {
-            request(url, function (error, response, body) {
+            request(options, function (error, response, body) {
+                
+                func(error,JSON.parse(body))
+            })            
+        }
+        else
+        {
+            return new Promise(function (resolve, reject) {
+                request(options, function (error, response, body) { 
+                    if(error)
+                        return reject(error)
+                    return resolve(JSON.parse(body))
+                }) 
+            })
+        }
+    }
+    get(url,data,header,func)
+    {
+        if(url.indexOf('?')==-1 && data)
+        {
+            url+="?"
+        }
+        if(data)
+            for(var a in data)
+            {
+                url+="&"+a+'='+data[a];
+            }
+        var options = { method: 'GET',
+            url: url, 
+            json: true };
+        if(header)
+            options.header=header;
+        if(func)
+        {
+            request(options, function (error, response, body) {
+                
                 func(error,body)
             })            
         }
         else
         {
             return new Promise(function (resolve, reject) {
-                request(url, function (error, response, body) {
+                request(options, function (error, response, body) { 
                     if(error)
                         return reject(error)
                     return resolve(body)
@@ -24,12 +73,45 @@ module.exports = class web
             })
         }
     }
-    post(url,data,func)    
+    postData(url,data,header,func)    
     {
         var options = { method: 'POST',
             url: url,
             body:data,
             json: true };
+        if(header)
+            options.header=header;
+            //console.log(options)
+        if(func)
+        {
+            request(options, function (error, response, body) {
+                //if (error) throw new Error(error);
+              
+                func(error,JSON.parse(body))
+              });
+                       
+        }
+        else
+        {
+            console.log(options)
+            return new Promise(function (resolve, reject) {
+                request(options, function (error, response, body) {
+                    if (error) return reject(error);
+                  
+                    return resolve(JSON.parse(body))
+                  });
+               
+            })
+        }        
+    }
+    post(url,data,header,func)    
+    {
+        var options = { method: 'POST',
+            url: url,
+            body:data,
+            json: true };
+        if(header)
+            options.header=header;
             //console.log(options)
         if(func)
         {
